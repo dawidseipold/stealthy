0b90cf133970a50c05733f4b782c92f8af3a67e2
dawidseipold <seipold.dawid@gmail.com>
2024-12-08 05:28:41 +0100

    feat: add SidebarButton component

---
+++ src/components/ui/sidebar/button/SidebarButton.tsx
@@ -0,0 +1,12 @@
+import { RequiredParentComponent } from "../../../../utils/types/component";
+
+interface SidebarButtonProps {
+}
+
+export const SidebarButton: RequiredParentComponent<SidebarButtonProps> = (props) => {
+  return (
+    <button class="rounded-2xl bg-white py-3 px-4 uppercase tracking-wide font-semibold">
+      {props.children}
+    </button>
+  )
+};
