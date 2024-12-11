9b4888f5e98aba8f01d3467a923c407afb6a1397
dawidseipold <seipold.dawid@gmail.com>
2024-12-11 01:37:17 +0100

    feat: add types to the conversations store and add an activeConversation store

--- src/utils/stores/conversations.ts
+++ src/utils/stores/conversations.ts
@@ -2,21 +2,51 @@
 
 import { createStore } from "solid-js/store";
 
+export type Attachment = {
+  type: "image" | "video" | "file";
+  name: string
+}
+
+export interface Message {
+  sender: string;
+  content: {
+    text: string;
+    attachment?: Attachment;
+  }
+}
+
 export interface Conversation {
   id: number,
   username: string;
   name: string;
+  messages: Message[];
 }
 
 export interface ConversationsStore {
   conversations: Conversation[];
 }
 
-
 export const [conversationsStore, setConversationsStore] = createStore<ConversationsStore>({
   conversations: [
-    { id: 0, username: "felix909", name: "chat 1" },
-    { id: 1, username: "tracy634", name: "chat 2" },
-    { id: 2, username: "johny123", name: "chat 3" }
+    {
+      id: 0, username: "bolton12", name: "chat 1", messages: [
+        { sender: "bolton12", content: { text: "Hey what's up?" } },
+        { sender: "quintine3", content: { text: "Nothing much. And you?" } },
+        { sender: "bolton12", content: { text: "Yeah, I'm good!" } },
+      ]
+    },
+    {
+      id: 1, username: "bolton12", name: "chat 2", messages: [
+        { sender: "bolton12", content: { text: "Hey what's up?" } },
+        { sender: "quintine3", content: { text: "Nothing much. And you?" } },
+        { sender: "bolton12", content: { text: "Yeah, I'm good!" } },
+      ]
+    },
   ]
 });
+
+
+export const [activeConversationStore, setActiveConversationStore] = createStore<{ activeConversation: Conversation | null }>({
+  activeConversation: null,
+});
+
