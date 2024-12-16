e3d17b6a9a35d9cd384346cf3b1c611e60ae9b2a
dawidseipold <seipold.dawid@gmail.com>
2024-12-16 06:36:35 +0100

    feat(backend): add a basic setup of the main file in the backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    stealthy_lib::run()
+mod db;
+mod errors;
+mod router;
+
+#[tokio::main]
+async fn main() {
+    stealthy_lib::run();
+
+    let database_layer = match db::setup_database().await {
+        Ok(layer) => layer,
+        Err(err) => {
+            eprintln!("Failed to set up the database: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    if let Err(err) = router::setup_server().await {
+        eprintln!("Failed to setup the server: {}", err);
+        std::process::exit(1);
+    }
 }
